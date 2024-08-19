961791d00c526eb5778c5e419301eb76a258e4ec
Aditi <aditideshpande2908@gmail.com>
2024-08-19 17:48:18 +0530

    set up components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css'
+import Home from './components/Home.jsx'
+import Clan from './components/Clan.jsx'
+import characters from './utilities/data.js'
+import { useState } from 'react'
 
 function App() {
-	return <> lets do some anime</>
+	const [data, setData] = useState(characters)
+	return (
+		<>
+			<Home data={data} />
+			<Clan data={data} />
+		</>
+	)
 }
 
 export default App
+
+// {
+//   name: 'Saitama',
+//   anime: 'One Punch Man',
+//   quote: "I'm just a guy who's a hero for fun.",
+//   hair: 'oops',
+//   price: 8.5,
+//   image: saitama,
+// },
