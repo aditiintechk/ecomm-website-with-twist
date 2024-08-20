9004242970202fd3039b0d80d8bc3f2929fc95b9
Aditi <aditideshpande2908@gmail.com>
2024-08-20 09:45:12 +0530

    added new json file with updated image urls

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,9 +5,14 @@ import { Hits, InstantSearch, SearchBox, Configure } from 'react-instantsearch'
 
 import { Hit } from './Hit'
 
-const searchClient = algoliasearch(
+/* const searchClient = algoliasearch(
 	'05BDRUAK9Q',
 	'426e3e46a593710ce7f74f434d414007'
+) */
+
+const searchClient = algoliasearch(
+	'OLS58B54A6',
+	'1e0911843ba71cec7d0585770610566b'
 )
 
 export const Search = () => {
