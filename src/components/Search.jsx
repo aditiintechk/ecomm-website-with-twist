b75c106dee10a90ed2a1bd6fae76133b0eb678ef
Aditi <aditideshpande2908@gmail.com>
2024-08-20 10:48:16 +0530

    add correct index name

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,24 +5,14 @@ import { Hits, InstantSearch, SearchBox, Configure } from 'react-instantsearch'
 
 import { Hit } from './Hit'
 
-/* const searchClient = algoliasearch(
-	'05BDRUAK9Q',
-	'426e3e46a593710ce7f74f434d414007'
-) */
-
-/* const searchClient = algoliasearch(
+const searchClient = algoliasearch(
 	'OLS58B54A6',
 	'1e0911843ba71cec7d0585770610566b'
-) */
-
-const searchClient = algoliasearch(
-	'IJJ6GWM0QK',
-	'945de06bf32563bd82f3a0b02d076a00'
 )
 
 export const Search = () => {
 	return (
-		<InstantSearch searchClient={searchClient} indexName='data'>
+		<InstantSearch searchClient={searchClient} indexName='data_New'>
 			<Configure hitsPerPage={25} />
 			<div className='ais-InstantSearch'>
 				<SearchBox placeholder='Search by Name, Anime Or Hair Color' />
